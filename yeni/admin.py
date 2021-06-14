b1ab11d13fb149f6e4879ed9a75431e3852d85fb
oxomn <oxomn@daum.net>
2021-06-14 18:16:06 +0900

    admin deco

--- yeni/admin.py
+++ yeni/admin.py
@@ -4,6 +4,21 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
+#StackedInline 대신에 TabularInline을 사용하면, 
+# 관련된 객체는 좀 더 조밀하고 테이블 기반 형식으로 표시
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+   
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date']
+admin.site.register(Question, QuestionAdmin)
