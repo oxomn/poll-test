01c5d1ab11e20bd97551c3db265851d340d507a3
oxomn <oxomn@daum.net>
2021-06-14 16:59:23 +0900

    bug test

--- yeni/views.py
+++ yeni/views.py
@@ -9,6 +9,7 @@ from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 #----------------------------------------
 class IndexView(generic.ListView):
     template_name = 'yeni/index.html'
